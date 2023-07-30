8ee426d8303c3bbd4217598904558a3d5d60db89
mrdbrg <rdg.brg@gmail.com>
2023-07-30 11:45:35 -0500

    add testing methods

---
+++ src/main/java/com/havefunwith/modules/demos/test/Calculator.java
@@ -0,0 +1,21 @@
+package com.havefunwith.modules.demos.test;
+
+import java.util.Arrays;
+
+public class Calculator {
+
+    // Convert it to Java Streams
+//    public int add(int ...numbers) {
+//        int sum = 0;
+//        for (int num : numbers) {
+//            sum += num;
+//        }
+//        return sum;
+//    }
+
+    public int add(int ...numbers) {
+        int sum = Arrays.stream(numbers).sum();
+        return sum;
+    }
+
+}

b91250e921c327fe3f84eb52d499b1825c21d04b
mrdbrg <rdg.brg@gmail.com>
2023-07-30 11:45:59 -0500

    change file location

--- src/test/java/com/havefunwith/test/CalculatorTest.java
+++ src/test/java/com/havefunwith/test/CalculatorTest.java
@@ -1,7 +1,9 @@
 package com.havefunwith.test;
 
+import com.havefunwith.modules.demos.test.Calculator;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.Assert;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
@@ -11,6 +13,31 @@ public class CalculatorTest {
         var number1 = 10;
         var number2 = 20;
         var result = underTest.add(number1, number2);
-        Assert.assertEquals(30, result);
+        assertEquals(30, result);
     }
+
+    @Test
+    void canHandleWhenInputIsZero() {
+        Calculator underTest = new Calculator();
+        var result = underTest.add(0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    void canHandleArrayOfPositiveNumbers() {
+        Calculator underTest = new Calculator();
+        int[] numArray = new int[]{2, 3, 6};
+        var result = underTest.add(numArray);
+        assertEquals(11, result);
+    }
+
+    @Test
+    @Disabled("Disable until bug SEILA#42 is fixed")
+    void canHandleArrayOfNegativeNumbers() {
+        Calculator underTest = new Calculator();
+        int[] numArray = new int[]{-2, -4, -6};
+        var result = underTest.add(numArray);
+        assertEquals(-12, result);
+    }
+
 }
